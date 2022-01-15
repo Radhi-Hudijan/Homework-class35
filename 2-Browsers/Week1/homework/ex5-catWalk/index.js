a1a2b5f95556fc6e7c2a95e4503b85c68db67fd1
Radhi Hudijan <radhif2009@gmail.com>
2022-01-15 13:43:19 +0100

    ex5 has been completed and tested

--- 2-Browsers/Week1/homework/ex5-catWalk/index.js
+++ 2-Browsers/Week1/homework/ex5-catWalk/index.js
@@ -21,8 +21,36 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Brow
 
    https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif
 -----------------------------------------------------------------------------*/
+const image = document.querySelector('img');
+const dancingCat =
+  'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+const walkingCat = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
+image.style.left = '0px';
+let x = 0;
+let count;
 function catWalk() {
-  // TODO complete this function
-}
+  x += 10;
+  image.style.left = x + 'px';
+  const screenWidth = window.innerWidth;
+  const catImageWidth = image.width;
 
-// TODO execute `catWalk` when the browser has completed loading the page
+  if (parseInt(image.style.left) >= screenWidth - catImageWidth) {
+    image.style.left = '0px';
+    count = false;
+  }
+
+  if (
+    !count &&
+    parseInt(image.style.left) >= (screenWidth - catImageWidth) / 2
+  ) {
+    count = true;
+    clearInterval(catMove);
+    image.src = dancingCat;
+    setTimeout(() => {
+      image.src = walkingCat;
+      setInterval(catWalk, 50);
+    }, 5000);
+  }
+}
+const catMove = setInterval(catWalk, 50);
+window.addEventListener('load', catWalk);
