da79380e801d5cc18caba84387350aa6f09440c5
Radhi Hudijan <radhif2009@gmail.com>
2022-01-17 19:07:09 +0100

    Ex6 has been completed and tested

--- 2-Browsers/Week1/homework/ex6-gameOfLife/index.js
+++ 2-Browsers/Week1/homework/ex6-gameOfLife/index.js
@@ -13,10 +13,12 @@ const NUM_ROWS = 40;
 // life or death
 function createCell(x, y) {
   const alive = Math.random() > 0.5;
+
   return {
     x,
     y,
     alive,
+    lifeTime: alive ? 1 : 0,
   };
 }
 
@@ -46,7 +48,17 @@ function createGame(context, numRows, numColumns) {
   // Draw a cell onto the canvas
   function drawCell(cell) {
     // Draw cell background
-    context.fillStyle = '#303030';
+    let opacity = 0;
+    if (cell.lifeTime === 1) {
+      opacity = 0.25;
+    } else if (cell.lifeTime === 2) {
+      opacity = 0.5;
+    } else if (cell.lifeTime === 3) {
+      opacity = 0.75;
+    } else if (cell.lifeTime >= 4) {
+      opacity = 1;
+    }
+    context.fillStyle = `#303030`;
     context.fillRect(
       cell.x * CELL_SIZE,
       cell.y * CELL_SIZE,
@@ -56,7 +68,7 @@ function createGame(context, numRows, numColumns) {
 
     if (cell.alive) {
       // Draw living cell inside background
-      context.fillStyle = `rgb(24, 215, 236)`;
+      context.fillStyle = `rgba(24, 215, 236, ${opacity})`;
       context.fillRect(
         cell.x * CELL_SIZE + 1,
         cell.y * CELL_SIZE + 1,
@@ -102,12 +114,19 @@ function createGame(context, numRows, numColumns) {
       if (numAlive === 2) {
         // Living cell remains living, dead cell remains dead
         cell.nextAlive = cell.alive;
+        cell.nextAlive ? cell.lifeTime++ : (cell.lifeTime = 0);
+        // if (cell.nextAlive) {
+        //   cell.lifeTime++;
+        // }
       } else if (numAlive === 3) {
         // Dead cell becomes living, living cell remains living
         cell.nextAlive = true;
+        // cell.lifeTime++;
+        cell.nextAlive ? cell.lifeTime++ : (cell.lifeTime = 1);
       } else {
         // Living cell dies, dead cell remains dead
         cell.nextAlive = false;
+        cell.lifeTime = 0;
       }
     });
 
